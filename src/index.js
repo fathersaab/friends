3e252705b1723fdf7dd4ecba6751d8e1369d7671
Nikhil Saini <saininikhil827@gmail.com>
2021-10-26 23:41:50 +0530

    First pr

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
-
+require('colors')
 const app = express();
 
 app.listen(3000, () => {
-	console.log("Running on port 3000");
+	console.log("Running on port 3000".yellow);
 });
